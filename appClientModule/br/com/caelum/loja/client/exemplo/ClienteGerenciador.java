e6c8c32535226847625604d4d3a9a762f7e178d7
jpfaria@gmail.com <jpfaria@gmail.com>
2013-06-25 17:54:53 -0300

    .

---
+++ appClientModule/br/com/caelum/loja/client/exemplo/ClienteGerenciador.java
@@ -0,0 +1,27 @@
+package br.com.caelum.loja.client.exemplo;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import br.com.caelum.loja.entity.Livro;
+import br.com.caelum.loja.session.GerenciadorLoja;
+
+public class ClienteGerenciador {
+
+	public static void main(String[] args) throws NamingException {
+
+		InitialContext ic = new InitialContext();
+
+		GerenciadorLoja gerenciador = (GerenciadorLoja) ic
+				.lookup("ejb:fj31-loja-ear/fj31-loja-ejb3/GerenciadorLojaBean!br.com.caelum.loja.session.GerenciadorLoja");
+
+		Livro livro = new Livro();
+		livro.setNome("Pais e filhos");
+		livro.setPreco(25);
+		
+		gerenciador.salva(livro);
+		
+
+	}
+
+}
